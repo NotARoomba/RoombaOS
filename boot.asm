6f0f445ce84ecc5bb9d3b7c65a5d081fe7a7cf23
NotARoomba <kg5inb1@hotmail.com>
2023-07-19 10:46:27 -0500

    Learning about the Stack

--- boot.asm
+++ boot.asm
@@ -1,33 +1,24 @@
 [org 0x7c00]
-mov ah, 0
-int 0x16
-mov bx, buffer
-mov [bx], al
-inc bx
-jmp bloop
-buffer:
-	times 10 db 0
-bloop:
-	mov ah, 0
-	int 0x16
-	cmp bx, buffer+10
-	je printBuffer
-	mov [bx], al
-	inc bx
-	jmp bloop
-printBuffer:
-	mov ah, 0x0e
-	mov bx, buffer
-	jmp ploop
-ploop:
+
+getInput:
+	mov bx, sp ; moves the current stack pointer to the base pointer
+	getInputLoop:
+		mov ah, 0
+		int 0x16
+		push ax
+		cmp al, 0x0D
+		je printInput
+		jmp getInputLoop
+printInput:
+	sub bx, 2 ; moving down (up) the stack 
 	mov al, [bx]
-	cmp bx, buffer+10
-	je end
+	mov ah, 0x0e
 	int 0x10
-	inc bx
-	jmp ploop
-			
-	
+	cmp al, 0x0D
+	je end
+	jmp getInput
+
+jmp getInput
 end:
 	jmp $
 times 510-($-$$) db 0
