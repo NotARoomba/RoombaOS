fe4d4cfb4a894096b6b5edfa9705b60ca3494240
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-07-22 18:21:37 -0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,18 +9,25 @@ void kmain() {
   //draw_text(0, 0, "Welcome to RoombaOS!", 0x0a);
   // Test mode test
   //print("start\n", 0x0a, -1, -1);
-  isr_install();
+    isr_install();
     irq_install();
-
-    printf("Type something, it will go through the kernel. Type END to halt the CPU\n> ");
+    print("Welcome to RoombaOS!\n", 0x0C, -1, -1);
+    printf("Type HELP for a list of commands\n> ");
 }
 
 void user_input(char *input) {
     if (strcmp(input, "END") == 0) {
         printf("Stopping the CPU. Bye!\n");
         asm volatile("hlt");
+    } else if(strcmp(input, "HELP") == 0) {
+      printf("List of Commands: \n");
+      printf("HELP - shows this menu\n");
+      printf("END - stops the CPU");
+    } else {
+      printf("\"");
+      printf(input);
+      printf("\"");
+      printf(" is not a command. Type HELP for a list of commands.");
     }
-    printf("You said: ");
-    printf(input);
     printf("\n> ");
 }
